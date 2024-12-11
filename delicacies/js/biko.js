77133a4bad474527af2b0a10d127543ac6cf3533
Kayla Gutlay <kaylagutlay08@gmail.com>
2024-12-11 18:33:44 +0800

    about us page and history page added

---
+++ delicacies/js/biko.js
@@ -0,0 +1,16 @@
+// JavaScript functionality (if needed in the future)
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Biko Recipe Page Loaded");
+});
+
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header');
+    var scrollPosition = window.scrollY;
+
+    // If the scroll position is near the header, add the darkened class
+    if (scrollPosition > 50) {  // You can adjust the value (50) to control when the effect starts
+        header.classList.add('darkened');
+    } else {
+        header.classList.remove('darkened');
+    }
+});
